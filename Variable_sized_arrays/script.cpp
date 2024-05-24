7eb6ec7ff484a49a26bb20e9e6ce8a4b9e418f01
_MR1_ <96888594+Marouane-Elgoumiri@users.noreply.github.com>
2024-05-24 19:32:13 +0100

    formated documents
    Co-Authored-By: Abidox808 <96650353+Abidox808@users.noreply.github.com>

--- Variable_sized_arrays/script.cpp
+++ Variable_sized_arrays/script.cpp
@@ -20,22 +20,26 @@ Sample Output
 #include <vector>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, q;
     cin >> n >> q;
 
     vector<vector<int>> arrays(n);
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         int k;
         cin >> k;
         arrays[i].resize(k);
-        for (int j = 0; j < k; ++j) {
+        for (int j = 0; j < k; ++j)
+        {
             cin >> arrays[i][j];
         }
     }
 
-    for (int i = 0; i < q; ++i) {
+    for (int i = 0; i < q; ++i)
+    {
         int a, b;
         cin >> a >> b;
         cout << arrays[a][b] << endl;
--- intersection_of_2_arrays/script.js
+++ intersection_of_2_arrays/script.js
@@ -4,15 +4,15 @@
 
 //  output=[4,7]
 
-var intersection = function(array1, array2) {
+var intersection = function (array1, array2) {
     let firstSet = new Set();
 
-    for(let num of array1){
+    for (let num of array1) {
         firstSet.add(num);
     }
     let intersectionSet = new Set();
-    for (let num of array2){
-        if (firstSet.has(num)){
+    for (let num of array2) {
+        if (firstSet.has(num)) {
             intersectionSet.add(num);
         }
     }
