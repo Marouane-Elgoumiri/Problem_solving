1994990ae4e915d44c1c6fcdce052b026a978329
_MR1_ <96888594+Marouane-Elgoumiri@users.noreply.github.com>
2024-12-09 09:29:30 +0000

    buzz buzz

--- Two Sums II/script.java
+++ Two Sums II/script.java
@@ -1,5 +1,26 @@
-package Two Sums II;
+// Source: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// O(n) time complexity
+// O(1) space complexity
 
 public class script {
-    
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        int[] result = twoSum(numbers, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+    public static int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        while(i < j){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target){
+                return new int[]{i+1, j+1};
+            } else if(sum < target){
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
--- Two Sums II/script.js
+++ Two Sums II/script.js
@@ -1,3 +1,6 @@
+// O(n) time complexity
+// O(1) space complexity
+
 var twoSum = function(numbers, target) {
     let a=0, b = numbers.length-1;
 
@@ -12,4 +15,18 @@ var twoSum = function(numbers, target) {
         }     
     }
     return [];
+
+    ``` 2 ms, 50.2 MB
+    const map = new Map();
+
+    for(let i=0; i<numbers.length; i++){
+        const complement = target - numbers[i];
+        if(map.has(complement)){
+            return [map.get(complement) + 1, i+1];
+        }
+        map.set(numbers[i],i);
+    }
+    return [];
+    `
+
 };
